0e6b46d545686e8bec866500d51514e4d522e0e7
Gichimu <gichimueric12@gmail.com>
2019-11-08 16:51:57 +0300

    add the credentials class

---
+++ credentials.py
@@ -0,0 +1,44 @@
+
+
+class Credentials:
+    '''
+    Class that defines attributes and default behaviour for the credentials class
+    '''
+    credentials = []
+
+    def __init__(self, account_name, username, password):
+        self.account_name = account_name
+        self.username = username
+        self.password = password
+
+    
+    def save_credential(self):
+        '''
+        Saving the credential in the credentials list
+        '''
+        
+        Credentials.credentials.append(self)
+
+
+    def delete_credential(self):
+        '''
+        Removing the credential fro the credentials list
+        '''
+
+        Credentials.credentials.remove(self)
+
+    @classmethod
+    def find_by_password(cls, password):
+        '''
+        check to see if an account is present in the credentials list
+
+        Args:
+            password: the password of the contact to be checked
+
+        Return:
+            returns the account if found or null if not found
+        '''
+
+        for credential in cls.credentials:
+            if(credential.password == password):
+                return credential
